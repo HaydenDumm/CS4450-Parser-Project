f838262ed20150bacb03dc4a362a69310439fdca
HaydenDumm <haydenadummerth@gmail.com>
2025-11-19 19:52:23 -0600

    Fixed a few things and changed the language of the lexer/parser/listener files to python

--- Deliverable_02/run_parse02_list.py
+++ Deliverable_02/run_parse02_list.py
@@ -3,7 +3,7 @@ from ParserProject02Lexer import ParserProject02Lexer
 from ParserProject02Parser import ParserProject02Parser
 from ParserProject02Listener import ParserProject02Listener
 
-class DumpAssignments(ParserProject01Listener):
+class DumpAssignments(ParserProject02Listener):
     def exitSimpleAssign(self, ctx:ParserProject02Parser.SimpleAssignContext):
         name = ctx.IDENT().getText()
         expr = ctx.expr().getText()
@@ -17,9 +17,9 @@ class DumpAssignments(ParserProject01Listener):
 
 def main():
     stream = FileStream("project_deliverable_2.py", encoding="utf-8")
-    lexer  = ParserProject01Lexer(stream)
+    lexer  = ParserProject02Lexer(stream)
     tokens = CommonTokenStream(lexer)
-    parser = ParserProject01Parser(tokens)
+    parser = ParserProject02Parser(tokens)
     tree   = parser.prog()
 
     walker = ParseTreeWalker()
